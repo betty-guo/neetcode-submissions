678a61689ee26085a253f3c21dda7fb9bb8975ef
Betty Guo <bettyg3113@gmail.com>
2026-06-30 13:53:52 -0400

    Add: python-string-slicing-part-1 - submission-1

---
+++ Python For Beginners/python-string-slicing-part-1/submission-1.py
@@ -0,0 +1,15 @@
+def get_substring(input_string: str, start: int, end: int) -> str:
+    # this works because the end index is non inclusive and the length of the str is also non inclusive (zero indexed strings)
+    if end > len(input_string):
+        return ""
+    return input_string[start: end]
+
+
+
+# do not modify below this line
+print(get_substring("NeetCode", 1, 7))
+print(get_substring("NeetCode", 1, 8))
+print(get_substring("NeetCode", 1, 9))
+print(get_substring("NeetCode", 0, 2))
+print(get_substring("NeetCode", 0, 7))
+print(get_substring("NeetCode", 4, 8))
