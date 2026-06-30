fd84f2b56bad2018790e3b7f2217385560db0943
Betty Guo <bettyg3113@gmail.com>
2026-06-30 12:00:36 -0400

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 25
+print(type(variable))
+variable = 25.0
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "25"
+print(type(variable))
+variable = [25]
+print(type(variable))
