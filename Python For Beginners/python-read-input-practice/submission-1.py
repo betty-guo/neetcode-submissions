1761f075be678f94b3aae33358f96dd787d0d3f3
Betty Guo <bettyg3113@gmail.com>
2026-06-30 20:00:36 -0400

    Add: python-read-input-practice - submission-1

---
+++ Python For Beginners/python-read-input-practice/submission-1.py
@@ -0,0 +1,12 @@
+def add_two_numbers() -> int:
+    line = input()
+    string_list = line.split(",")
+    return int(string_list[0]) + int(string_list[1])
+
+
+
+# do not modify below this line
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
