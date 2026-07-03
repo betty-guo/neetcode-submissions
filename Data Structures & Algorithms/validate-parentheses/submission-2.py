43c005b2bc50c105cc10af9bf8929c2fbbb47e0a
Betty Guo <bettyg3113@gmail.com>
2026-07-03 17:56:17 -0400

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # use a stack, if its an opening bracket add it to the stack
+        # if its a closing one, pop from the stack it should match, otherwise return false
+        # if get to the end and stack is empty, return true.
+        # otherwise return false, there is some unmatched opening brackets still
+
+        matching_brackets = {
+            ')': '(',
+            '}': '{',
+            ']': '['
+        }
+        stack = []
+        for c in s:
+            if c == '(' or c == '{' or c == '[':
+                stack.append(c)
+            else: # is a closing bracket
+                if not stack: # stack is empty, no opening brackets
+                    return False
+                elif stack[-1] != matching_brackets[c]: # peek at top
+                    return False
+                else:
+                    stack.pop()
+
+        return len(stack) == 0
