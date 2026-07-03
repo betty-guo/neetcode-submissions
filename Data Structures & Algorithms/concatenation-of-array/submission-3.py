b47aa89b6cf9dc1fe9d122b3c86787e0e65217b0
Betty Guo <bettyg3113@gmail.com>
2026-07-03 17:16:46 -0400

    Add: concatenation-of-array - submission-3

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-3.py
@@ -0,0 +1,15 @@
+# input: nums List[int], of length n
+# output: ans List[int], of length 2n
+# ans[i] == nums[i], ans[i + n] == nums[i] for 0 <= i <n
+# e.g. we stack nums on top of itself twice
+
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        # return nums + nums # basic python
+
+        n = len(nums)
+        ans = [0] * 2 * n # init array first
+        for i in range(len(nums)):
+            ans[i] = ans[i+n] = nums[i]
+        return ans
+
