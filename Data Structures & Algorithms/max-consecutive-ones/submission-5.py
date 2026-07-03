925e850b9e43a42fee254690f5f2b326ece3498a
Betty Guo <bettyg3113@gmail.com>
2026-07-03 15:37:06 -0400

    Add: max-consecutive-ones - submission-5

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-5.py
@@ -0,0 +1,39 @@
+# You are given a binary array nums, return the maximum number of consecutive 1's in the array.
+# input: List[int] nums
+# output: int max_num_consecutive_ones
+
+# Example
+# input: nums = [1,1,0,1,1,1]
+# output: 3
+
+# assume nums can't be null
+# assume nums can be empty
+# assume nums can contain no 1s
+
+# test case with no 1s
+# test case ending on a 0
+# test case ending on a 1
+# test case where all 1s
+
+# space complexity: O(1), only constants, no additional memory.
+# time complexity: O(n), one pass through array
+class Solution: 
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        
+        max_num_consecutive_ones = 0
+        ones_counter = 0
+
+        for num in nums:
+            if num == 1: # already counting 1s
+                ones_counter += 1
+                # need to increment if we end on a 1, will miss this case
+                max_num_consecutive_ones = max(max_num_consecutive_ones, ones_counter)
+            elif ones_counter > 0: # not a 1
+                max_num_consecutive_ones = max(max_num_consecutive_ones, ones_counter)
+                ones_counter = 0 # reset to 0
+
+        return max_num_consecutive_ones
+        
+
