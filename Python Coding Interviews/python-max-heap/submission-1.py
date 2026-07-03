8a0254b12f993e14197e4205e0ae1e96c671624d
Betty Guo <bettyg3113@gmail.com>
2026-07-03 00:13:10 -0400

    Add: python-max-heap - submission-1

---
+++ Python Coding Interviews/python-max-heap/submission-1.py
@@ -0,0 +1,22 @@
+import heapq
+from typing import List
+
+
+def get_reverse_sorted(nums: List[int]) -> List[int]:
+    negative = [-n for n in nums] # reverse first
+    heapq.heapify(negative) # heapify. this means negative[0] will have the smallest number, which is negative of the biggest number
+    output = []
+
+    while negative:
+        output.append(-heapq.heappop(negative))
+
+    return output
+
+
+
+
+
+# do not modify below this line
+print(get_reverse_sorted([1, 2, 3]))
+print(get_reverse_sorted([5, 6, 4, 2, 7, 3, 1]))
+print(get_reverse_sorted([5, 6, -4, 2, 4, 7, -3, -1]))
