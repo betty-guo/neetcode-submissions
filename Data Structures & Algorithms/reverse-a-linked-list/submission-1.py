58db95ac854956731b1aa0cd7bd518242873c8b8
Betty Guo <bettyg3113@gmail.com>
2026-07-04 16:20:47 -0400

    Add: reverse-a-linked-list - submission-1

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-1.py
@@ -0,0 +1,41 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+# input: head ListNode
+# output: newHead ListNode
+
+# Example:
+# Input: head = [0,1,2,3]
+
+# Output: [3,2,1,0]
+
+# edge cases:
+# head can be null
+# input can be output (e.g. the listNode is size one)
+# the linked list may be circular (e.g. will come back to head at one point)
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+     # brute force
+     # iterate through linked list and build a new linked list by keeping track of the 
+     # nodes behind and forward, and setting curr.next = previous, and then jumping to the original curr.next
+
+        if head:
+            start_node = head
+            previous_node = None
+            next_node = None
+            curr = head
+            while curr: # in case it is circular, curr == start_node at the end
+                next_node = curr.next # keep path to next node
+                curr.next = previous_node # set current pointer to one behind
+                previous_node = curr # set the new previous to the one you are currently at
+                curr = next_node # advance
+                if curr == start_node:
+                    curr.next = previous_node
+                    return curr
+            return previous_node
+        else:
+            return None # Optional is either a ListNode or None
