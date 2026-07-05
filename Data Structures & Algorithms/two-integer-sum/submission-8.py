4240b8ff5b62ead9b777131e78dd9b6b60b3b65f
Betty Guo <bettyg3113@gmail.com>
2026-07-04 21:07:00 -0400

    Add: two-integer-sum - submission-8

---
+++ Data Structures & Algorithms/two-integer-sum/submission-8.py
@@ -0,0 +1,29 @@
+# input: 
+# array of integers nums
+# integer target
+
+# output
+# indices i and j such that nums[i]+nums[j] == target
+# i != j
+# return smaller index first
+
+# Example
+# Input: 
+# nums = [3,4,5,6], target = 7
+
+# Constraints:
+# no null (nums.length >= 2)
+# integers can be negative
+# only one valid answer
+# i != j
+
+# Output: [0,1]
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        valMap = {} # map to store target - value: index, for reverse lookup. Then don't need to iterate through O(N^2) time e.g. brute force
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in valMap:
+                return [valMap[diff], i]
+            valMap[num] = i
+            
