363d2105194be36bd9f347c25695f8cc58b754d8
Betty Guo <bettyg3113@gmail.com>
2026-07-04 20:55:55 -0400

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,27 @@
+# input s and t
+# output true if strings are anagrams (e.g. same letters) otherwise return false
+
+# example
+# Input: s = "racecar", t = "carrace"
+
+# Output: true
+
+
+# example 2
+# Input: s = "jar", t = "jam"
+
+# Output: false
+
+# edge cases
+# both s and t are not null, at least length of 1
+# s and t are lowercase (capitalization doesn't matter)
+# it's not an anagram if not all of the letters are used
+
+from collections import Counter
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool: # O(n + m) since iterate through both strings, space complexity is O(1) since at most 26 characters
+        if len(s) != len(t):
+            return False
+        
+        return sorted(s) == sorted(t)
+        
