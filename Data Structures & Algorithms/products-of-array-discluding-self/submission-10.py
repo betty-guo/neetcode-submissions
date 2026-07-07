c3c0fe9fd49ee0bba4ff06363e1bdf97962ddbe8
Betty Guo <bettyg3113@gmail.com>
2026-07-07 19:56:30 -0400

    Add: products-of-array-discluding-self - submission-10

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-10.py
@@ -0,0 +1,33 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        # # can we use prefix sum somehow? e.g. array where arr[i] = product of every element up to and including i
+        # # we can use prefix sum and suffix sum e.g. two arrays, product up to element i (not including), suffix is product of all elements i+1 to end
+        # # so productExceptSelf = prefix[i] * suffix[i]
+        # # works around division, so don't need to handle 0s
+        # O(n) time and space, since pass through nums twice and then iterate another time to create result. Create intermediate arrays of length n
+
+        # n = len(nums)
+        # # prefix/ suffic not including i would have no numbers, leave as 1 so doesn't affect result
+        # prefix_product = [1] * n
+        # suffix_product = [1] * n
+
+        # for i in range(1, n):
+        #     prefix_product[i] = nums[i - 1] * prefix_product[i-1]
+        
+        # for i in range(n - 2, -1, -1): # go backwards
+        #     suffix_product[i] = nums[i + 1] * suffix_product[i + 1] # we want the product to be product of everything not including the current i
+
+        # return [ i * j for i, j in zip(prefix_product, suffix_product)]
+        n = len(nums)
+        res = [0] * n
+        pref = [0] * n
+        suff = [0] * n
+
+        pref[0] = suff[n - 1] = 1
+        for i in range(1, n):
+            pref[i] = nums[i - 1] * pref[i - 1]
+        for i in range(n - 2, -1, -1):
+            suff[i] = nums[i + 1] * suff[i + 1]
+        for i in range(n):
+            res[i] = pref[i] * suff[i]
+        return res
