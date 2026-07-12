8c954124919f2beeb1c3e5415aa47d9c7d1fbdfd
Betty Guo <bettyg3113@gmail.com>
2026-07-12 10:43:18 -0700

    Add: evaluate-reverse-polish-notation - submission-9

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-9.py
@@ -0,0 +1,32 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        # input: tokens won't be empty, will be valid RPN and num in [-200, 200]
+        #   need to be careful if num == 0 because division by 0
+        # output: result of the evaluation of the expression
+
+        # use a stack to apply the operations
+
+        # edge cases
+
+        RPN = []
+
+        for token in tokens:
+            if token == "+":
+                op2 = RPN.pop()
+                op1 = RPN.pop()
+                RPN.append(op1 + op2)
+            elif token == "-":
+                op2 = RPN.pop()
+                op1 = RPN.pop()
+                RPN.append(op1 - op2)
+            elif token == "*":
+                op2 = RPN.pop()
+                op1 = RPN.pop()
+                RPN.append(op1 * op2)
+            elif token == "/":
+                op2 = RPN.pop()
+                op1 = RPN.pop()
+                RPN.append(int(op1 / op2)) # floor, truncate to 0
+            else: # is an integer
+                RPN.append(int(token))
+        return RPN.pop() # should only have one value left
