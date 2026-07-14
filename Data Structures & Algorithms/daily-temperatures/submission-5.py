c9489f68c96ebde83c105aa1bfe0dd59d74fdad9
Betty Guo <bettyg3113@gmail.com>
2026-07-13 22:31:35 -0700

    Add: daily-temperatures - submission-5

---
+++ Data Structures & Algorithms/daily-temperatures/submission-5.py
@@ -0,0 +1,17 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        s = []
+        res = [0] * len(temperatures)
+
+        for i, t in enumerate(temperatures):
+            # look at the top of the stack
+            # if its smaller, you just the next warmest day
+            # use its index to compute the diff vs current temp day (num days untl found warmer day)
+            # pop from stack
+            # keep going until you found a higher temp, then add to stack
+            while s and s[-1][1] < t:
+                index, temp = s.pop()
+                res[index] = i - index
+            s.append((i, t)) # add to stack
+
+        return res
